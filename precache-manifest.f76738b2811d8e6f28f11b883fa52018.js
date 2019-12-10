8a346bb2b93dec0ff60615aa1b30d9dbd9a7c0b7
Kotelnikov Igor <digikot1996@gmail.com>
2019-12-10 16:54:58 +0200

    Updates

---
+++ precache-manifest.f76738b2811d8e6f28f11b883fa52018.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b3b12f3607c5dc24335b979b8477c6",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "e419145c046961418973",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.951a195d.chunk.css"
+  },
+  {
+    "revision": "6fe75fba430425464629",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.4b4776e3.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.4b4776e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e419145c046961418973",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.d032f08a.chunk.js"
+  },
+  {
+    "revision": "5898b43781fcd94828d3",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.8d80df19.js"
+  }
+]);
